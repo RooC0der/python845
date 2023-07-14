98e053602f529f01cdc04d74a19090920c826b03
RooC0der <roo.gonzalez845@gmail.com>
2023-07-13 22:49:05 -0600

    RooC0der
    clases pasadas

---
+++ repaso.py
@@ -0,0 +1,4 @@
+#pedir a un usuario 2 numeros 
+my_number1 = int (input ('escribe un numero: '))
+my_number2 = int (input('escribe tu segundo numero:'))
+print('tu primer numero', my_number1, 'es mayor que', my_number2, '=',  (my_number1 > my_number2) )
