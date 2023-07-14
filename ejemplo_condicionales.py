87a58d1ccf13c13ceb6080a7645c79dc146db57c
RooC0der <roo.gonzalez845@gmail.com>
2023-07-14 00:19:29 -0600

    @RooC0der
    Condicionales

---
+++ ejemplo_condicionales.py
@@ -0,0 +1,23 @@
+a = 8888;
+b = 85;
+d = False;
+e = 'helo world';
+f = 'atar a la rata';
+
+if a > b:
+    print ('el numero mayor es', a )
+else:
+    print('el numero mayor es', b ) 
+
+
+#ejercicio 2
+c = input('te gusta la rata?')
+
+if c == 'yes':
+    print ('atar a la rata')
+elif c == 'not':
+    print('somos o no somos')
+
+else:
+    print ('anita lava la tina')    
+    
---
+++ if_ejercicio.py
@@ -0,0 +1,6 @@
+#paso de antro
+a = int(input('Que edad tienes para entrar a ala choza de los pequeñines?'))
+if  0<= 18:
+    print ('Ahí la bimbo dijo Wonder')
+else:
+    print('nelson le dijo a wilson ')
